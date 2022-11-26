1d3870401ea22be366b3994ea9e96920fc8da105
Yavor Fingarov <yavor.fingarov@outlook.com>
2022-11-26 20:17:19 +0200

    Added code analysis (#28)

--- TodoTxtDaemon/Program.cs
+++ TodoTxtDaemon/Program.cs
@@ -7,7 +7,7 @@ namespace TodoTxtDaemon
         public static void Main()
         {
             var host = Host.CreateDefaultBuilder()
-                .UseContentRoot(Path.GetDirectoryName(typeof(Program).Assembly.Location) ?? Directory.GetCurrentDirectory())
+                .UseContentRoot(Path.GetDirectoryName(typeof(Program).Assembly.Location)!)
                 .ConfigureAppConfiguration(configuration =>
                 {
                     configuration.Sources.Clear();
